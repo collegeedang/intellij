bb7509538b38313f2a99bfd3b2627452df7abc4d
collegee <jeondaehak@gmail.com>
2022-11-01 23:09:21 +0900

    commit test

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,14 @@
+package ch01.sec11;
+
+/**
+ * @author eogkr
+ */
+/*
+장제목:1장 자바 시작하기
+작성일:2022.10.20
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,Java");
+    }
+}
---
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args){
+
+        int age = 15;
+
+        int result = age +12;
+        System.out.println(result);
+
+    }
+}
